6a053abec574bac20b64592d5d3beae5ec533bef
Anastasiia-Domanska <akvadelf@gmail.com>
2025-05-22 21:10:13 +0200

    change in js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let count = 0;
-
 const screen = document.querySelector('.screen');
 const increaseBtn = document.querySelector('.increase');
 const decreaseBtn = document.querySelector('.decrease');
+
+let count = 0;
 
 increaseBtn.addEventListener('click', () => {
   count++;
